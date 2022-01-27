98462e778033378b13de71949af4bde31e2df6c3
MHuesler <hueslermaxim@gmail.com>
2022-01-27 23:08:25 +0100

    added favicon, Sites section

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,13 @@ import type { NextPage } from 'next'
 import Footer from '../components/Footer'
 import Nav from '../components/Nav'
 import Landing from '../sections/Landing'
+import Websites from '../sections/Websites'
 
 const Home: NextPage = () => {
   return (
     <>
       <Landing></Landing>
+      <Websites></Websites>
       <Footer></Footer>
     </>
   )
